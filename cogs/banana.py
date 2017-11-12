c385cd127cb364e858cd8ba0b59101d36223f95b
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-11-12 11:43:49 -0500

    Update banana.py

--- cogs/banana.py
+++ cogs/banana.py
@@ -14,6 +14,10 @@ class banana:
     @commands.command()
         async def ping(self, ctx)
             """Playing Ping Pong. Returning your websocket latency."""
+            em = discord.Embed()
+            em.title = 'Pong! This is your websocket latency:'
+            em.description = f"{bot.ws.latency * 1000:.4f} ms"
+            await ctx.send(embed=em)
         
         
 def setup(bot): 
